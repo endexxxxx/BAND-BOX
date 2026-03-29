2b5f84520dc0eaf02a018c1f2a1cdf9ac754826c
차서후 <cha34301629@gmail.com>
2026-03-29 09:28:36 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+// Smooth scroll for navigation links
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    document.querySelector(targetId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
